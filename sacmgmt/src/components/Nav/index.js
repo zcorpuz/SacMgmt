28d1e1d55b318143044e41ce008bfcc6dc2893fd
Cattleya Pia <cjpia612@gmail.com>
2020-09-10 07:06:57 -0700

    added styling through creating css file

---
+++ sacmgmt/src/components/Nav/index.js
@@ -0,0 +1,32 @@
+import React, {Component} from "react";
+import {NavItems} from "./NavItems";
+import "./style.css";
+
+class Nav extends Component {
+  state = { clicked: false}
+
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked})
+  }
+
+  render() {
+    return (
+      <nav className="NavbarItems">
+        <div className= "menu-icon" onClick={this.handleClick}>
+        </div>
+        <ul className= {this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
+          {NavItems.map((item, index) => {
+            return(
+              <li key={index}>
+                <a className={item.class} href={item.url}>{item.label}</a>
+              </li>
+            )
+          })}
+        </ul>
+      </nav>
+    );
+  }
+  
+}
+
+export default Nav;
