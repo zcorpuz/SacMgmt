3ea4a92ac0bf0af7479b05a5aaad8446b9ede584
Cattleya Pia <cjpia612@gmail.com>
2020-09-10 21:49:07 -0700

    fixed Team modal to appear properly

--- sacmgmt/src/App.js
+++ sacmgmt/src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
-import './App.css';
+// import './App.css';
 import Nav from "./components/Nav/index";
 import { Contact } from './components/Contact/Contact';
-import { Aboutus } from './components/Contact/Aboutus';
+import Aboutus from './components/Contact/Aboutus';
+import Team from './components/Contact/Team';
 
 function App() {
   return (
     <div>
     <Nav />
      <Aboutus />
+      <Team />
      <Contact /> 
     </div>
   );
