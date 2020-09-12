c09571ee6fafcb0c1eb55f29a9ba4fd2f21c1c11
Caroaldana <19Aldana@gmail.com>
2020-09-12 10:47:33 -0700

    Changes to Team

--- sacmgmt/src/components/Contact/Team.js
+++ sacmgmt/src/components/Contact/Team.js
@@ -2,30 +2,14 @@ import React, { useEffect, useState, useRef } from 'react'
 
 
 function Team() {
-    const outside = useRef()
     const [isOpen, setIsOpen] = useState(false)
   
-    const handleClick = e => {
-      if (outside.current.contains(e.target)) {
-        return
-      }
-      setIsOpen(false)
-    }
-  
-    useEffect(() => {
-      const getClick = document.addEventListener('click', handleClick)
-  
-      return () => {
-        getClick()
-      }
-    }, [])
-  
     return (
-      <div className="Team" ref={outside}>
+      <div className="Team">
         <button onClick={() => setIsOpen(!isOpen)}>Meet Our Team</button>
         {isOpen ? (
           <div className="modal">
-            <p>Modal is open!!!</p>
+            <p>Modal is open!!!!</p>
           </div>
         ) : null}
       </div>
