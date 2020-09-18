218cfb0aff31d23e915e6129196be351a6941aad
Zach Corpuz <zachcorpuz@yahoo.com>
2020-09-17 20:44:52 -0700

    Updated package.json to take out unnecessary dependency. Updated server.js file for mongoose database and created a seed.js file

--- models/contact.js
+++ models/contact.js
@@ -1,14 +1,19 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-// require('mongoose-type-email');
-
-// mongoose.SchemaTypes.Email.defaults.message = 'Email address is invalid'
-
 const contactSchema = new Schema({
-  name: { type: String, required: true },
-  // email: [{type: mongoose.SchemaTypes.Email, required: true}],
-  message: { type: String },
+  name: { 
+    type: String, 
+    required: true 
+  },
+  email: {
+    type: String,
+    match: [/.+@.+\..+/, "Please enter a valid e-mail address"],
+    required: true
+  },
+  message: { 
+    type: String,
+  },
 });
 
 const Contact = mongoose.model("Contact", contactSchema);
