e4a87dafecbf6017ac203aab789fb4c767183a27
Zach Corpuz <zachcorpuz@yahoo.com>
2020-09-18 22:32:52 -0700

    Fixed and styled fonts throughout the website

--- client/src/components/Aboutus/Aboutus.js
+++ client/src/components/Aboutus/Aboutus.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./style.css";
 
-import Team from "../Team/Team";
+// import Team from "../Team/Team";
 
 class Aboutus extends Component {
     render() {
