13587789ca7109350fde96681d2cc0ec45c1c103
mairagee524 <garciamaira524@yahoo.com>
2021-02-27 02:24:55 -0800

    fixed index routing issues

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Nav from "./components/Nav/index";
+import Nav from "./components/Nav";
 import ServiceSection from "./components/ServiceSection";
 import serviceData from "./serviceData";
 import Hero from "./components/Hero";
 // import Team from './components/Team/Team';
 import Contact from './components/Contact';
-import Aboutus from "./components/Aboutus/index";
+import Aboutus from "./components/AboutUs";
 // import Teammember from './components/Team/Teammember';
 
 function App() {
