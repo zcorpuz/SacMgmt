4c2f15f5ed69a7fd436215601fedf6de2d895ed9
mairagee524 <garciamaira524@yahoo.com>
2021-02-27 12:13:51 -0800

    changed folder structure

--- client/src/components/Team/index.js
+++ client/src/components/Team/index.js
@@ -16,19 +16,5 @@ function Team(props) {
       </div>
     </div>
   )
-
-
-  // const [isOpen, setIsOpen] = useState(false);
-
-  // return(
-  //   <div className="team">
-  //     <button type="submit" onClick={() =>
-  //     {setIsOpen(!isOpen)}}> <a id="smoothscroll" className="smoothscroll" href="#teamMembers">Meet the Team!</a></button>
-  //     {isOpen ?
-  //       <div className="modal-team">
-  //       </div>
-  //      : null}
-  //   </div>
-  // )
 }
 export default Team;
